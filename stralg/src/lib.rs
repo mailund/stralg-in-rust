c9e83a409cc9d0f7ec154915ad4b7429c714e5fc
Thomas Mailund <tm@kvantify.dk>
2025-02-10 19:36:16 +0100

    Refactored tests

--- stralg/src/lib.rs
+++ stralg/src/lib.rs
@@ -3,6 +3,5 @@ pub mod search;
 
 pub use patterns::border_array;
 pub use patterns::strict_border_array;
-pub use search::border_search;
 pub use search::kmp;
 pub use search::naive;
--- stralg/src/search/mod.rs
+++ stralg/src/search/mod.rs
@@ -61,104 +61,6 @@ pub fn naive<'a>(x: &'a str, p: &'a str) -> impl Iterator<Item = usize> + 'a {
     })
 }
 
-pub struct BorderSearch<'a> {
-    /// The string we are searching in
-    x: &'a str,
-    /// The pattern we are searching for
-    p: &'a str,
-    /// The border array of the pattern
-    border_array: Vec<usize>,
-    /// The current index in the string
-    x_index: usize,
-    /// The current index in the pattern
-    p_index: usize,
-}
-
-impl<'a> BorderSearch<'a> {
-    pub fn new(x: &'a str, p: &'a str) -> Self {
-        let b = strict_border_array(p);
-        BorderSearch {
-            x,
-            p,
-            border_array: b,
-            x_index: 0,
-            p_index: 0,
-        }
-    }
-}
-
-impl<'a> Iterator for BorderSearch<'a> {
-    type Item = usize;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let BorderSearch {
-            x,
-            p,
-            border_array: b,
-            x_index: i,
-            p_index: j,
-            ..
-        } = self;
-
-        let n = x.len();
-        let m = p.len();
-        while *i < n {
-            // Shift pattern until it matches the border
-            while *j > 0 && &x[*i..*i + 1] != &p[*j..*j + 1] {
-                *j = b[*j - 1];
-            }
-
-            // Move one step forward (if we can)
-            if &x[*i..*i + 1] == &p[*j..*j + 1] {
-                *j += 1;
-            }
-
-            // Move to the next position (saving it for the next call to next)
-            *i += 1;
-
-            // Return if a match was found
-            if *j == m {
-                *j = b[*j - 1];
-                return Some(*i - m);
-            }
-        }
-        None
-    }
-}
-
-/// Returns an iterator over the starting indices of occurrences of the pattern
-/// `p` in the text `x` using the border search algorithm.
-///
-/// The border search algorithm uses the border array to efficiently find
-/// occurrences of the pattern `p` in the text `x`. It shifts the pattern
-/// according to the border array to avoid unnecessary comparisons.
-///
-/// The algorithm runs in O(n) time, where `n` is the length of the text.
-///
-/// # Arguments
-///
-/// * `x` - The text in which to search for the pattern.
-/// * `p` - The pattern to search for.
-///
-/// # Returns
-///
-/// An iterator over the starting indices of occurrences of the pattern `p` in
-/// the text `x`.
-///
-/// # Examples
-///
-/// ```
-/// use stralg::border_search;
-///
-/// let text = "abracadabra";
-/// let pattern = "abr";
-/// let matches: Vec<usize> = border_search(text, pattern).collect();
-/// assert_eq!(matches, vec![0, 7]);
-/// ```
-pub fn border_search<'a>(x: &'a str, p: &'a str) -> BorderSearch<'a> {
-    BorderSearch::new(x, p)
-}
-
 struct KMPSearch<'a> {
     /// The string we are searching in
     x: &'a str,
@@ -232,7 +134,8 @@ impl Iterator for KMPSearch<'_> {
 /// the pattern `p` in the text `x`. It shifts the pattern according to the
 /// border array to avoid unnecessary comparisons.
 ///
-/// The algorithm runs in O(n) time, where `n` is the length of the text.
+/// The algorithm runs in O(n + m) time, where `n` is the length of the text, and
+/// `m` is the length of the pattern.
 ///
 /// # Arguments
 ///
@@ -273,18 +176,31 @@ mod tests {
         assert_eq!(result, vec![0, 7]);
     }
 
-    #[test]
-    fn naive_01() {
-        search_01(naive);
+    fn search_02<'a, F, I>(f: F)
+    where
+        F: Fn(&'a str, &'a str) -> I,
+        I: Iterator<Item = usize> + 'a,
+    {
+        let x = "aaaaa";
+        let p = "aa";
+        let result: Vec<usize> = f(x, p).collect();
+        assert_eq!(result, vec![0, 1, 2, 3]);
     }
 
-    #[test]
-    fn border_search_01() {
-        search_01(border_search);
+    macro_rules! search_tests {
+        ($($test_name:ident: $search_fn:expr,)*) => {
+            $(
+                #[test]
+                fn $test_name() {
+                    search_01($search_fn);
+                    search_02($search_fn);
+                }
+            )*
+        }
     }
 
-    #[test]
-    fn kmp_01() {
-        search_01(kmp);
+    search_tests! {
+        naive_tests: naive,
+        kmp_tests: kmp,
     }
 }
