0879d20b8250b3fc93d5806b5837f45e0f0c3eaa
Thomas Mailund <tm@kvantify.dk>
2025-02-10 14:47:25 +0100

    fixed strict_border_array

--- stralg/src/lib.rs
+++ stralg/src/lib.rs
@@ -11,39 +11,93 @@ mod search {
         })
     }
 
-    fn border_array(p: &str) -> Vec<usize> {
+    /// Computes the border array for the given pattern `p`.
+    ///
+    /// The border array is an array where the value at each index `i` is the length of the longest
+    /// proper prefix of the substring `p[0..=i]` which is also a suffix of this substring.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use stralg::border_array;
+    ///
+    /// let pattern = "abracadabra";
+    /// let borders = border_array(pattern);
+    /// assert_eq!(borders, vec![0, 0, 0, 1, 0, 1, 0, 1, 2, 3, 4]);
+    /// ```
+    ///
+    /// ```
+    /// use stralg::border_array;
+    ///
+    /// let pattern = "aaaa";
+    /// let borders = border_array(pattern);
+    /// assert_eq!(borders, vec![0, 1, 2, 3]);
+    /// ```
+    ///
+    /// ```
+    /// use stralg::border_array;
+    ///
+    /// let pattern = "abcd";
+    /// let borders = border_array(pattern);
+    /// assert_eq!(borders, vec![0, 0, 0, 0]);
+    /// ```
+    pub fn border_array(p: &str) -> Vec<usize> {
         let m = p.len();
-        let mut b = vec![0; m];
+        let mut ba = vec![0; m];
         let mut j = 0;
         for i in 1..m {
             while j > 0 && &p[i..i + 1] != &p[j..j + 1] {
-                j = b[j - 1];
+                j = ba[j - 1];
             }
             if &p[i..i + 1] == &p[j..j + 1] {
                 j += 1;
             }
-            b[i] = j;
+            ba[i] = j;
         }
-        b
+        ba
     }
 
-    /*
-    fn filter_border_array(ba: Vec<usize>) -> Vec<usize> {
-        for (j, b) in ba.iter().enumerate() {
-            if j == ba.len() {
-                break;
-            }
-            if *b > 0 {
-                let mut k = j - 1;
-                while k > 0 {
-                    if ba[k] == *b {
-                        ba[k] = 0;
-                    }
-                    k -= 1;
-                }
+    /// Computes the strict border array for the given pattern `p`.
+    ///
+    /// The strict border array `ba` is an array where the value at each index `i` is the length of the longest
+    /// proper prefix of the substring `p[0..=i]` which is also a suffix of this substring, with the additional
+    /// constraint that `p[ba[i]+1] != p[i+1]`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use stralg::strict_border_array;
+    ///
+    /// let pattern = "abracadabra";
+    /// let borders = strict_border_array(pattern);
+    /// assert_eq!(borders, vec![0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 4]);
+    /// ```
+    ///
+    /// ```
+    /// use stralg::strict_border_array;
+    ///
+    /// let pattern = "aaaa";
+    /// let borders = strict_border_array(pattern);
+    /// assert_eq!(borders, vec![0, 0, 0, 3]);
+    /// ```
+    ///
+    /// ```
+    /// use stralg::strict_border_array;
+    ///
+    /// let pattern = "abcd";
+    /// let borders = strict_border_array(pattern);
+    /// assert_eq!(borders, vec![0, 0, 0, 0]);
+    /// ```
+    pub fn strict_border_array(p: &str) -> Vec<usize> {
+        let mut ba = border_array(p);
+        for j in 1..(ba.len() - 1) {
+            let b = ba[j];
+            if b > 0 && p[b..b + 1] == p[j + 1..j + 2] {
+                ba[j] = ba[b - 1];
             }
         }
-    }*/
+        ba
+    }
 
     pub struct BorderSearch<'a> {
         /// The string we are searching in
@@ -60,7 +114,7 @@ mod search {
 
     impl<'a> BorderSearch<'a> {
         pub fn new(x: &'a str, p: &'a str) -> Self {
-            let b = border_array(p);
+            let b = strict_border_array(p);
             BorderSearch {
                 x,
                 p,
@@ -247,6 +301,8 @@ mod search {
     }
 }
 
+pub use search::border_array;
 pub use search::border_search;
 pub use search::kmp;
 pub use search::naive;
+pub use search::strict_border_array;
